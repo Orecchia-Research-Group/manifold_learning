3a59e5ffb6464ef20f4d57023698e432c8f8eb77
robbobbinett <robinettryan8@gmail.com>
2020-06-30 11:26:49 -0400

    working on building simplicial complex from set of full-dimensional simplices

--- tests/test_triangulation.py
+++ tests/test_triangulation.py
@@ -1,5 +1,16 @@
-from triangulation import AugmentedDelaunay
+from triangulation import power_set_bar_empty, AugmentedDelaunay
 import numpy as np
+
+def test_power_set_bar_empty():
+	test_tuple = (1, 2, 3, 4)
+	test_output = power_set_bar_empty(test_tuple)
+	try:
+		assert test_output == [(1,), (2,), (3,), (4,), (1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4), (1, 2, 3), (1, 2, 4), (1, 3, 4), (2, 3, 4), (1, 2, 3, 4)]
+	except AssertionError:
+		raise ValueError
+
+def test_all_faces_from_d_simplices():
+	pass
 
 def test_delaunay_formation():
 	points = np.load("data/delaunay_test.npy")
--- triangulation.py
+++ triangulation.py
@@ -1,5 +1,26 @@
 import numpy as np
 from scipy.spatial import Delaunay
+from itertools import chain, combinations
+
+def power_set_bar_empty(some_tuple):
+	"""
+	Given a tuple, returns all nonempty,
+	sorted subtuples of that list, using the ordering
+	of the original tuple for sorting
+	"""
+	# Modified from Mark Rushakoff's answer to
+	# the following StackOverflow question:
+	# https://stackoverflow.com/questions/1482308/how-to-get-all-subsets-of-a-set-powerset
+	return list(chain.from_iterable(combinations(some_tuple, r) for r in range(1, len(some_tuple)+1)))
+
+def all_faces_from_d_simplices(simplices):
+	assert all(map(lambda x: len(x) == len(simplices[0]), simplices))
+	face_set = set()
+	for simplex in simplices:
+		faces = power_set_bar_empty(simplex)
+		for face in faces:
+			face_set.add(face)
+	return face_set
 
 class AugmentedDelaunay:
 	def __init__(self, points, point_names):
@@ -12,3 +33,13 @@ class AugmentedDelaunay:
 		assert isinstance(point_names, list)
 		assert all(isinstance(x, int) for x in point_names)
 		assert len(point_names) == self.size
+		self.point_names = point_names
+
+	def get_simplices(self):
+		simplices = []
+		original_simplices = self.delaunay.simplices
+		for simplex in original_simplices:
+			temp = map(lambda x: point_names[x], simplex)
+			temp.sort()
+			simplices.append(tuple(temp))
+		return simplices
