140fefe8e6c99f8276f29283b74254c747dbe66f
robbobbinett <robinettryan8@gmail.com>
2020-07-21 15:53:19 -0400

    testing sampling of points from hypersphere

---
+++ tests/test_mSVD.py
@@ -0,0 +1,13 @@
+from manifold_utils.mSVD import hypersphere, eigen_calc
+import numpy as np
+
+def test_hypersphere():
+	"""
+	Test that hypersphere genuinely samples points
+	uniformly from the surface of the unit hypersphere
+	"""
+	n = 10000
+	for d in [3, 5, 7]:
+		points = hypersphere(n, d)
+		for j in range(n):
+			assert np.isclose(np.linalg.norm(points[:, j]), 1)
