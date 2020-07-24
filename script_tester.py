f5a25cda22fd467af1832c34c5cb336315bc2531
Kyle Ruark <kyleruark@Kyles-MacBook-Pro-2.local>
2020-07-24 15:46:24 -0500

    WOOO

--- script_tester.py
+++ script_tester.py
@@ -2,24 +2,46 @@ from manifold_utils.manifold_sampling import sample_grassmann as sg
 from manifold_utils.mSVD import eigen_calc as ec
 from manifold_utils.mSVD import eigen_plot as ep
 from manifold_utils.mSVD import eps_projection
+from manifold_utils.mSVD import hypersphere
 import numpy as np
 import random
 
 
+
+## Recreate figure 1
+hs=hypersphere(1000,10)
+hs=np.stack(hs, axis=1)
+#print(hs)
+print(np.shape(hs))
+zeros_mat=np.zeros((1000,90))
+print(np.shape(zeros_mat))
+new_hs=np.append(hs,zeros_mat,axis=1)
+#print(new_hs)
+#print(np.shape(new_hs))
+hs_noisy=new_hs+np.random.randn(1000,100)*.1
+print(hs_noisy)
+dim_mat=np.shape(hs_noisy)
+rng=random.randint(0,dim_mat[0])
+center=hs_noisy[rng,:]
+eigval_list,top_eigvecs,X_mat = ec(hs_noisy,center,9,1.1,2.2,.01)
+ep(eigval_list,1.1,2.2,.01)
+
+
+
 ## Obtain and reshape  cloud point matrix (N x D) from sample_grassmann function
-cloud=sg(4,3,1000)
-cloud_mat=np.reshape(cloud,(1000,12))
+#cloud=sg(4,3,1000)
+#cloud_mat=np.reshape(cloud,(1000,12))
 #print(np.shape(cloud))
 #print(np.shape(cloud_mat))
 #print(cloud_mat)
 
 ## Pick a random point to act as the center of the sphere
-dim_mat=np.shape(cloud_mat)
-rng=random.randint(0,dim_mat[0])
-center=cloud_mat[rng,:]
+#dim_mat=np.shape(cloud_mat)
+#rng=random.randint(0,dim_mat[0])
+#center=cloud_mat[rng,:]
 
 ## Feed cloud_mat and center into the eigen_calc function
-eigval_list,top_eigvecs,X_mat=ec(cloud_mat,center,2,.1,1,.01)
+#eigval_list,top_eigvecs,X_mat=ec(cloud_mat,center,2,.1,1,.01)
 #print(np.array(eigval_list))
 #print(np.shape(eigval_list))
 #print(np.array(top_eigvecs))
@@ -31,7 +53,7 @@ eigval_list,top_eigvecs,X_mat=ec(cloud_mat,center,2,.1,1,.01)
 #print(np.shape(top_eigvecs[90]))
 
 ##Plug into projection function
-eigvecs=top_eigvecs[90]
+#eigvecs=top_eigvecs[90]
 #projection=eps_projection(X_mat,eigvecs,center,2)
 #print(projection)
 
