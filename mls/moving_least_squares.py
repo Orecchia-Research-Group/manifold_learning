15f35e0ed7867a70c0b494b860c2a6a60f03fdc3
ideclue <isabelladc92@gmail.com>
2020-07-29 18:24:23 -0400

    Testing weight functions

--- mls/moving_least_squares.py
+++ mls/moving_least_squares.py
@@ -2,6 +2,47 @@ import numpy as np
 import scipy 
 import matplotlib.pyplot as plt
 
+
+#Defining the weight function
+def weight(x):
+    if (x <= -1) or (x >= 1):
+        return 0
+    elif (x < -1/2):
+        return 2*((x+1)**2)
+    elif (x < 1/2):
+        return -2*(x**2) + 1
+    elif (x < 1):
+        return 2*((x-1)**2)
+
+def weight_scaled(x,delta):
+    return weight(x/delta)
+
+def dweight(x):
+    if (x <= -1) or (x >= 1):
+        return 0
+    elif (x < -1/2):
+        return 4*(x+1)
+    elif (x < 1/2):
+        return (-4*x)
+    elif (x < 1):
+        return 4*(x-1)
+
+def dweight_scaled(x,delta):
+    return dweight(x/delta)
+
+def ddweight(x):
+    if (x <= -1) or (x >= 1):
+        return 0
+    elif (x < -1/2):
+        return 4
+    elif (x < 1/2):
+        return -4
+    elif (x < 1):
+        return 4
+
+def ddweight_scaled(x,delta):
+    return ddweight(x/delta)
+
 class C_1_MLS_oracle:
 	"""
 	The purpose of this class is to take a set of points (x_i,y_i)
@@ -17,20 +58,6 @@ class C_1_MLS_oracle:
 			chapter 4 of Wendland)
 		delta: float, represents scaling factor
 		"""
-
-		#Generating weight function: cubic spline
-		x = np.linspace(-1,1,100)
-		y1 = -2*np.power(x,2) + 1
-		y2=2*np.power(np.add(x,1),2)
-		y3=2*np.power(np.subtract(x,1),2)
-		y = np.concatenate((y2[0:25],y1[25:75],y3[75:100]))
-		from scipy.interpolate import interp1d
-
-		#w(x) returns the value of the weight function for a value x in [-1,1]
-		w = interp1d(x, y, kind='cubic')
-
-
-
 
 
 
