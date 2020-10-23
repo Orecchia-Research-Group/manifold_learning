8f6f6d7428410ffd28fe874adb95aa6ebf2b3c11
Kyle Ruark <kyleruark@Kyles-MacBook-Pro-2.local>
2020-10-23 12:58:04 -0500

    Smapled, need to get eigvecs

--- quadratic_fit_test.py
+++ quadratic_fit_test.py
@@ -1,26 +1,21 @@
-from manifold_utils.mSVD import hypersphere
-from combined_mls_pca import mls_pca
-from fit_quad_diff import manifold_regression
 import numpy as np
-import math
-import sklearn
-import random
+import matplotlib.pyplot as plt
 
-## Create S^9 Embedded in R^100
-hs=hypersphere(1000,10)
-hs=np.stack(hs, axis=1)
-zeros_mat=np.zeros((1000,90))
-new_hs=np.append(hs,zeros_mat,axis=1)
-hs_noisy=new_hs+np.random.randn(1000,100)*math.sqrt(.1)
-dim_mat=np.shape(hs_noisy)
+## Sample within unit circle R^2
+length = np.sqrt(np.random.uniform(0,1,400))
+print(length)
 
-## Run mls_pca on it
-center_ind=random.randrange(0,dim_mat[0])
-eigvals,eigvecs,radii,Rmin,Rmax,points,eig_perp = mls_pca(hs_noisy,center_ind,9)
-print(np.shape(eigvecs))
-print(np.shape(eig_perp))
+theta = np.pi * np.random.uniform(0,2,400)
+print(theta)
 
+x=length * np.cos(theta)
+y=length * np.sin(theta)
+z=np.sqrt(1-x**2-y**2)
 
-## Perform quadratic fitting
-#manifold_regression(eigvecs[-1,],eig_perp[-1,],points,hs_noisy[center_ind,])
+plt.scatter(x,y)
+plt.show()
+
+table=np.stack((x,y,z),axis=1)
+
+print(table)
 
