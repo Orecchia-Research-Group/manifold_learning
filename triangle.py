7c368c0938d3af45656bd3d0fe00a662ead61402
ideclue <isabelladc92@gmail.com>
2021-01-22 00:00:27 -0600

    Added sierpinski triangle iteration

--- triangle.py
+++ triangle.py
@@ -14,29 +14,34 @@ def midpoint(point1, point2):
     return ((point1[0] + point2[0]) / 2, (point1[1] + point2[1]) / 2)
 
 def call_triangle(num_iterations, node1 = (0.0,0.0), node2 = (2.0,0.0), node3 = (1.0,1.0)):
-    # Creating simple triangle
     output_points = triangle(num_iterations, [node1, node2, node3])
     final_points = []
     for count, point in enumerate(output_points):
         if count % 3 == 0:
             final_points.append(output_points[count:count + 3])
-    final_graph = create_graph(final_points)
-    print(final_graph.nodes())
+    final_graph = create_graph2(final_points)
     draw_triangle(final_graph)
 
-def create_graph(points):
+def create_graph2(points):
     my_graph = nx.Graph()
-    counter = 0
+    seen = {}
+    count = 0
     for point_set in points:
-        new_graph = nx.Graph()
-        new_graph.add_node(0, pos = point_set[0])
-        new_graph.add_node(1, pos = point_set[1])
-        new_graph.add_node(2, pos = point_set[2])
-        new_graph.add_edges_from([(0,1),(1,2),(2,0)])
-        my_graph = nx.disjoint_union(new_graph, my_graph)
+        curr_nodes = []
+        for point in point_set:
+            if point not in seen.keys():
+                my_graph.add_node(count, pos = point)
+                seen[point] = count
+                curr_nodes.append(count)
+                count += 1
+            else:
+                curr_nodes.append(seen[point])
+        my_graph.add_edges_from([(curr_nodes[0], curr_nodes[1]),(curr_nodes[1], curr_nodes[2]),(curr_nodes[2], curr_nodes[0])])
     return my_graph
 
 def draw_triangle(graph):
     positions = nx.get_node_attributes(graph, 'pos')
+    nodes = graph.nodes()
+    to_remove = []
     nx.draw(graph, positions)
     pass
