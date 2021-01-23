0e28984905bb44bbb59496554ad799087b767ac3
robbobbinett <robinettryan8@gmail.com>
2021-01-22 20:02:39 -0500

    little progress

--- patch_play.py
+++ patch_play.py
@@ -3,17 +3,18 @@ from sklearn.metrics.pairwise import euclidean_distances
 from time import time
 import PatchExtractTools as pet
 from combined_mls_pca import rapid_mls_pca
+from manifold_utils.mSVD import Sparse_eigen_calc_from_dist_mat_uncentered as eigen_calc
 
 #load matrix
-Patches = np.load('Denoised3x3Patches.npy')
-dist_mat = euclidean_distances(Patches)
+patches = np.load('Denoised3x3Patches.npy')
+dist_mat = euclidean_distances(patches)
 #do ev plot around c given by cid
 cid = 5000
-#Image_ev_plot(Patches, dist_mat, cid)
 
-start_time = time()
-rapid_mls_pca(Patches, cid, 2, dist=dist_mat)
-runtime = time() - start_time
+start = time()
+radii, _, eigval_list, eigvec_list = eigen_calc(patches, dist_mat, cid, np.max(dist_mat), k=5, radint=0.1)
+np.save("data/natural_images/radii_"+str(cid)+".npy", np.array(radii))
+np.save("data/natural_images/eigvals_"+str(cid)+".npy", np.stack(eigval_list, axis=0))
+np.save("data/natural_images/eigvecs_"+str(cid)+".npy", np.stack(eigvec_list, axis=0))
 
-with open("timestamp.txt", "w") as f:
-	f.write(str(runtime))
+print("Time elapsed: "+str(time() - start))
