2a763c98f57c8390f8968408e7bf719661c98d87
robbobbinett <robinettryan8@gmail.com>
2021-02-11 03:47:23 -0500

    working on passing witness complex to Eirene

--- witness_complex.jl
+++ witness_complex.jl
@@ -1,6 +1,6 @@
 using Combinatorics
 
-function weak_witness_complex(witness_dist_mat, max_deg::Int64)
+function weak_witness_complex_simplices(witness_dist_mat, max_deg::Int64)
 	# n is the number of landmarks
 	# N is the number of non-landmark points
 	n, N = size(witness_dist_mat)
@@ -14,8 +14,9 @@ function weak_witness_complex(witness_dist_mat, max_deg::Int64)
 	# edge is witnessed by each non-landmark
 	edges = Set{Tuple}()
 	for j in 1:N
-		sorted_col = sort(witness_dist_mat[:, j])
-		push!(edges, Tuple(sorted_col[1:2]))
+		column = witness_dist_mat[:, j]
+		closest_landmarks = sort(collect(1:n), by=x->column[x])
+		push!(edges, Tuple(closest_landmarks[1:2]))
 	end
 	simplices[1] = edges
 
@@ -44,17 +45,90 @@ function weak_witness_complex(witness_dist_mat, max_deg::Int64)
 	return simplices
 end
 
+function array_lt(array1::Array{Int64, 1}, array2::Array{Int64, 1})
+	# iterate over indices until array1 is not equal
+	# to array2 at some index
+	ind = 1
+	while true
+		item1 = array1[ind]
+		item2 = array2[ind]
+		if item1 == item2
+			ind += 1
+		else
+			return item1 < item2
+		end
+	end
+end
+
+function simplices_to_Eirene_complex(simplices::Dict{Int64, Set}, max_deg::Int64)
+	# Start by sorting j-simplices for k in 0:max_deg
+	sorted_simplices = Dict{Int64, Array}()
+	for j in 0:max_deg
+		list_of_simplices = collect(simplices[j])
+		if j == 0
+			sorted_simplices[j] = sort(list_of_simplices)
+		else
+			sorted_simplices[j] = sort(list_of_simplices, lt=array_lt)
+		end
+	end
+
+	# Put all simplices into single array
+	simplex_array = Array{Union{Int64, Tuple}, 1}()
+	for j in 0:max_deg
+		for item in sorted_simplices[j]
+			push!(simplex_array, item)
+		end
+	end
+
+	# Store number of simplices
+	n_simplices = length(simplex_array)
+
+	# Map each simplex to its new array index
+	simplex_inds = Dict{Union{Int64, Tuple}, Int64}()
+	for j in 1:n_simplices
+		println(simplex_array[j])
+		simplex_inds[simplex_array[j]] = j
+	end
+
+	# Form "codimension-1" matrix
+	D = zeros(Int8, n_simplices, n_simplices)
+	for j in n_simplices
+		simplex = simplex_array[j]
+		if isa(simplex, Tuple)
+			len_simplex = length(simplex)
+			facets = combinations(simplex, len_simplex-1)
+			# Account for when 1-simplices has vertex-boundaries
+			if len_simplex == 2
+				for facet in facets
+					k = simplex_inds[facet[1]]
+					D[j, k] = 1
+				end
+			# General case
+			else
+				for facet in facets
+					k = simplex_inds[facet]
+					D[j, k] = 1
+				end
+			end
+		end
+	end
+
+	# Get S, rv, cp per Eirene documentation
+	S = sparse(D)
+	rv = S.rowval
+	cp = S.colptr
+
+	return D
+end
+
 arr = [1 2 3;
 	3 4 5;
 	5 6 7;
 	8 9 10]
 
 max_deg = 3
-simplices = weak_witness_complex(arr, max_deg)
-for j in 0:max_deg
-	println("Simplices of degree "*string(j)*":")
-	current_simplices = simplices[j]
-	for x in current_simplices
-		println(x)
-	end
-end
+simplices = weak_witness_complex_simplices(arr, max_deg)
+
+D = simplices_to_Eirene_complex(simplices, max_deg)
+
+print(D)
