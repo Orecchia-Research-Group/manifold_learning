2de322d133335c7ef25892a187a48cb51d234a79
robbobbinett <robinettryan8@gmail.com>
2021-02-16 00:29:23 -0500

    playing with gudhi SimplexTree methods

--- gudhi_play.py
+++ gudhi_play.py
@@ -1,3 +1,4 @@
+from time import time
 import numpy as np
 import gudhi
 
@@ -8,5 +9,7 @@ for j in range(3):
 	for k in range(simplices.shape[0]):
 		st.insert(simplices[k, :])
 
-print(st.num_simplices())
-print(st.num_vertices())
+st.compute_persistence(homology_coeff_field=2)
+start = time()
+print(st.betti_numbers())
+print(time() - start)
