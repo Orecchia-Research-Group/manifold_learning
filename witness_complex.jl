67b4d4073868a9336fc5d7f280b41541bdcbb0c4
robbobbinett <robinettryan8@gmail.com>
2021-02-15 22:24:38 -0500

    successfully making and writing complexes; fixed bug in making complexes

--- witness_complex.jl
+++ witness_complex.jl
@@ -1,11 +1,9 @@
 using HDF5
 using Combinatorics
 
-function intersect_iterable(iter_obj::Array)
-	return_obj = iter_obj[1]
-	for j in 2:length(return_obj)
-		return_obj = intersect(return_obj, iter_obj[j])
-	end
+function union_iterable(iter_obj::Array)
+	return_obj = Set{Tuple{Vararg{Int64}}}()
+	union!(return_obj, iter_obj)
 	return Tuple(return_obj)
 end
 
@@ -45,8 +43,8 @@ function weak_witness_complex_simplices(witness_dist_mat, max_deg::Int64)
 				end
 			end
 			if cond
-				intersection = intersect_iterable(combo)
-				push!(new_simplices, intersection)
+				onion = union_iterable(combo)
+				push!(new_simplices, onion)
 			end
 		end
 		simplices[j] = new_simplices
@@ -55,7 +53,22 @@ function weak_witness_complex_simplices(witness_dist_mat, max_deg::Int64)
 	return simplices
 end
 
+function simplices_to_HDF5(simplices::Dict{Int64, Set}, max_deg::Int64, filename::String)
+	touch(filename)
+	fid = h5open(filename, "w")
+	for j in 0:max_deg
+		write(fid, string(j), string(collect(simplices[j])))
+	end
+	close(fid)
+end
+
 arr = h5read("data/sample_witness_slice.h5", "witness_slice")
 
-max_deg = 3
-simplices = weak_witness_complex_simplices(arr, max_deg)
+#arr = [1 2 3 4 5;
+#	0.5 1.5 2.5 3.5 4.5]
+
+#max_deg = 3
+max_deg = 2
+@time simplices = weak_witness_complex_simplices(arr, max_deg)
+
+simplices_to_HDF5(simplices, max_deg, "data/sample_weak_witness.h5")
