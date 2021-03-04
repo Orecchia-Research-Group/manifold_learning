a3ecb875e03618eafec747ef3b296345c5f3780b
robbobbinett <robinettryan8@gmail.com>
2021-03-04 07:47:08 -0500

    more complete witness complex formation for ILC data

--- witness_complex.jl
+++ witness_complex.jl
@@ -58,8 +58,8 @@ function simplices_to_npy(simplices::Dict{Int64, Set}, max_deg::Int64, file_pref
 	end
 end
 
-# ks = (1, 5, 10, 50, 100, 500, 1000, 5000, 10000)
-ks = (5,)
+ks = (1, 5, 10, 50, 100, 500, 1000, 5000, 10000)
+# ks = (5,)
 ps = (0.01, 0.05, 0.1, 0.2)
 pps = (0.05, 0.1, 0.2)
 
@@ -69,10 +69,10 @@ for p in ps
 			toople = (k, p, pp)
 			arr = npzread("data/sample_witness_slice_kis"*string(toople)*".npy")
 
-			max_deg = 3
+			max_deg = 1
 			simplices = weak_witness_complex_simplices(arr, max_deg)
 
 			simplices_to_npy(simplices, max_deg, "data/sample_weak_witness_kis"*string(toople))
-		end
+			end
 	end
 end
