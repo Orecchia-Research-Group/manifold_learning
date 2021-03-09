08c4cec766a19b9e34af1105947d0eb7adb6ef56
robbobbinett <robinettryan8@gmail.com>
2021-03-09 14:15:52 -0500

    circle_tracer as a Jupyter notebook through jupytext

--- circle_tracer.py
+++ circle_tracer.py
@@ -1,3 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: py:light,ipynb
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.10.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
 import numpy as np
 import pandas as pd
 import scanpy as sc
