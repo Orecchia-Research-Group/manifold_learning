69b2c967d25a33550ce501d61e794bfa8160cf85
robbobbinett <robinettryan8@gmail.com>
2021-03-10 12:03:03 -0500

    loading in miRNA data

--- circadian.py
+++ circadian.py
@@ -5,7 +5,4 @@ import pickle
 df = pd.read_csv("data/circadian/matrix/series_matrix.txt",
 	sep=None, engine="python", index_col="ID_REF")
 
-row_names = df.index.tolist()
-
-for name in row_names:
-	print(name)
+miRNA_mat = df.to_numpy().T
